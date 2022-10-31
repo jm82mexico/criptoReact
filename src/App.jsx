0c7c12ecf657dbdc8d6c0d56ed3af50a9a11c782
jm82mexico <jmdelan2012@gmail.com>
2022-10-30 22:15:18 -0600

    Estilos botón formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import styled from '@emotion/styled'
 import ImagenCripto from './img/imagen-criptos.png'
+import Formulario from './components/Formulario'
 
 const Contenedor = styled.div`
   max-width:900px;
@@ -51,6 +52,7 @@ function App() {
       ></Imagen>
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario/>
       </div>
     </Contenedor>
   )
