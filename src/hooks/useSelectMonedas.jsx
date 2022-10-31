b267744273d01c0b5beeb8d7df9a03d3003303a2
jm82mexico <jmdelan2012@gmail.com>
2022-10-30 22:24:00 -0600

    Hook personalizado monedas inicial

---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const useSelectMonedas = () => {
+    const SelectMonedas = () =>{
+        console.log('Desde Select Monedas')
+    }
+
+    return [SelectMonedas]
+}
+
+export default useSelectMonedas
